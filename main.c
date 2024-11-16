18c525ad409e5ad29e13dc2b381ba8be23f1c0b3
0113coin <0113coin@gmail.com>
2024-11-16 13:02:24 +0900

    main.c

--- main.c
+++ main.c
@@ -27,24 +27,6 @@ void textcolor(int colorNum) {
 	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), colorNum);
 }
 
-enum ColorType{
-BLACK,  	//0
-darkBLUE,	//1
-DarkGreen,	//2
-darkSkyBlue,    //3
-DarkRed,  	//4
-DarkPurple,	//5
-DarkYellow,	//6
-GRAY,		//7
-DarkGray,	//8
-BLUE,		//9
-GREEN,		//10
-SkyBlue,	//11
-RED,		//12
-PURPLE,		//13
-YELLOW,		//14
-WHITE		//15
-} COLOR;
 
 // 메인 함수
 int main() {
