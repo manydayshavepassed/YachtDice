d0cc64c155047578d1651699f703a577008af6cb
manydayshavepassed <psj2195@naver.com>
2024-11-30 00:40:49 +0900

    Update main.c
    checkFourOfAKind함수 수정
    반환 값을 i * 4에서 (i + 1) * 4로 조정

--- main.c
+++ main.c
@@ -161,7 +161,7 @@ int checkFourOfAKind(int dice[]) {
     }
     for (int i = 0; i < 6; i++) {
         if (counts[i] >= 4) {
-            return i * 4;
+            return (i + 1) * 4;
         }
     }
     return 0;
